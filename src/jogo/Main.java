d2363b47c1a018b5bf6f81a3cf28afcf97614770
Pedro Henrique Machado Bastos <phmachado@gmail.com.br>
2022-11-05 09:16:06 -0300

    menu Image

--- src/jogo/Main.java
+++ src/jogo/Main.java
@@ -1,9 +1,12 @@
 package jogo;
 
+import jplay.GameImage;
+import jplay.URL;
 import jplay.Window;
 
 public class Main {
 	public static void main(String[] args) {
 		Window janela = new Window(800, 600);
+		GameImage plano = new GameImage(URL.sprite("menu.png"));
 	}
 }
