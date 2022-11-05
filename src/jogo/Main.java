e328a5dd0b00e19ef44d92a66822f3a3aa40e340
Pedro Henrique Machado Bastos <phmachado@gmail.com.br>
2022-11-05 10:35:59 -0300

    keyboard enter

--- src/jogo/Main.java
+++ src/jogo/Main.java
@@ -1,6 +1,7 @@
 package jogo;
 
 import jplay.GameImage;
+import jplay.Keyboard;
 import jplay.URL;
 import jplay.Window;
 
@@ -8,5 +9,15 @@ public class Main {
 	public static void main(String[] args) {
 		Window janela = new Window(800, 600);
 		GameImage plano = new GameImage(URL.sprite("menu.png"));
+		Keyboard teclado = janela.getKeyboard();
+		
+		while(true) {
+			plano.draw(); // draw the menu.png in screen
+			janela.update();
+			
+			if(teclado.keyDown(Keyboard.ENTER_KEY)) {
+				
+			}
+		}
 	}
 }
