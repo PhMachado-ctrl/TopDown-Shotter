61e6150ff53be5dcf8c8e8f509fae935b4682862
Pedro Henrique Machado Bastos <phmachado@gmail.com.br>
2022-11-11 09:16:41 -0300

    run function

--- src/jogo/cenario1.java
+++ src/jogo/cenario1.java
@@ -1,5 +1,25 @@
 package jogo;
 
-public class cenario1 {
+import jplay.Scene;
+import jplay.URL;
+import jplay.Window;
 
+public class cenario1 {
+	private Window janela;
+	private Scene cena;
+	
+	public void Cenario1(Window window) {
+		janela = window;
+		cena = new Scene();
+		cena.loadFromFile(URL.scenario("cenario1.png"));
+		
+		run(); //função contem o loop infinito
+	}
+
+	private void run() {
+		while(true) {
+			cena.draw();
+		}
+		
+	}
 }
